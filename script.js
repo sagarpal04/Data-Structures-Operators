3a4f0617c5760fdbd680bcf2d99df8e489e6f017
sagarpal04 <sagarpal0402@gmail.com>
2024-02-20 10:07:48 +0530

    Destructuring Objects

--- script.js
+++ script.js
@@ -5,33 +5,82 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
-// const arr = [2, 3, 4];
-// const [x, y, z] = arr;
-// console.log(x, y, z);
-// // const [first, second] = restaurant.categories;
-// // console.log(first, second);
-// // const [first, , second] = restaurant.categories;
-// // console.log(first, second);
-// let [main, , secondary] = restaurant.categories;
-// // Switching Variable
-// [main, secondary] = [secondary, main];
-// console.log(main, secondary);
-// // Receive 2 return values from a function
-// const [starter, mainCourse] = restaurant.order(2, 0);
-// console.log(starter, mainCourse);
 
-// // Nested destructuring
-// const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j);
-// const [i, , [j, k]] = nested;
-// console.log(i, j, k);
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-// // Default values
-// // const [p, q, r] = [8, 9];
-// const [p = 1, q = 1, r = 1] = [8, 9];
-// console.log(p, q, r);
+restaurant.orderDelivery({
+  address: "Via del sole, 21",
+  starterIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Changing Variable Name
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = {
+  a: 23,
+  b: 7,
+  c: 14,
+};
+({ a, b } = obj);
+console.log(a, b);
+
+const { fri } = openingHours;
+console.log(fri);
+
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+let {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
