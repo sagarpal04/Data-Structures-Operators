be2959aa1091a36eebf47382ad81594ee34b10c5
sagarpal04 <sagarpal0402@gmail.com>
2024-02-20 09:38:06 +0530

    Destructuring Arrays

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+// const arr = [2, 3, 4];
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// // const [first, second] = restaurant.categories;
+// // console.log(first, second);
+// // const [first, , second] = restaurant.categories;
+// // console.log(first, second);
+// let [main, , secondary] = restaurant.categories;
+// // Switching Variable
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+// // Receive 2 return values from a function
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// // Nested destructuring
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// // Default values
+// // const [p, q, r] = [8, 9];
+// const [p = 1, q = 1, r = 1] = [8, 9];
+// console.log(p, q, r);
