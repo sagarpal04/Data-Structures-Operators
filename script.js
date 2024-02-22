38edccdaf96626823bb00d931d3dc5510403c9f4
sagarpal04 <sagarpal0402@gmail.com>
2024-02-22 19:20:24 +0530

    Coding Challenge #2

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
 "use strict";
-
-// 111 Coding Challenge #1
-// Coding Challenge #1
-// We're building a football betting app (soccer for my American friends �)!
-// Suppose we get data from a web service about a certain game ('game' variable on
-// next page). In this challenge we're gonna work with that data.
-// Your tasks:
-// 1. Create one player array for each team (variables 'players1' and
-// 'players2')
-// 2. The first player in any player array is the goalkeeper and the others are field
-// players. For Bayern Munich (team 1) create one variable ('gk') with the
-// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
-// field players
-// 3. Create an array 'allPlayers' containing all players of both teams (22
-// players)
-// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
-// new array ('players1Final') containing all the original team1 players plus
-// 'Thiago', 'Coutinho' and 'Perisic'
-// 5. Based on the game.odds object, create one variable for each odd (called
-// 'team1', 'draw' and 'team2')
-// 6. Write a function ('printGoals') that receives an arbitrary number of player
-// names (not an array) and prints each of them to the console, along with the
-// number of goals that were scored in total (number of player names passed in)
-// 7. The team with the lower odd is more likely to win. Print to the console which
-// team is more likely to win, without using an if/else statement or the ternary
-// operator.
-// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
-// Then, call the function again with players from game.scored
-
 const game = {
   team1: "Bayern Munich",
   team2: "Borrussia Dortmund",
@@ -67,31 +38,34 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
-  printGoals(...playersScored) {
-    console.log(`${playersScored.length} goals were scored`);
-    console.log(...playersScored);
-  },
 };
 // 1
-const [players1, players2] = game.players;
-console.log(players1);
-console.log(players2);
+for (const [index, element] of game.scored.entries()) {
+  console.log(`Goal ${index + 1} : ${element}`);
+}
 // 2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk);
-console.log(fieldPlayers);
+let average = 0;
+const values = Object.values(game.odds);
+for (const value of values) {
+  average += value;
+}
+average /= values.length;
+console.log(average);
 // 3
-const [allPlayers] = [...players1, ...players2];
-console.log(allPlayers);
+for (const [team, value] of Object.entries(game.odds)) {
+  const str = team == "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${str} : ${value}`);
+}
+const scorers = {};
+const sagar = {
+  age: 19,
+};
 // 4
-const plater1Final = [...players1, "Thiago", "Coutinho", "Peristic"];
-console.log(plater1Final);
-// 5
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, draw, team2);
-// 6
-game.printGoals("Davies", "Muller", "Lewandowski", "Kinmich");
-game.printGoals(...game.scored);
-// 7
-game.odds.team1 > game.odds.team2 && console.log("Teams 1 Wins");
-game.odds.team1 < game.odds.team2 && console.log("Teams 2 Wins");
+const scoredPlayers = game.scored;
+for (const player of scoredPlayers) {
+  if (scorers[player]) {
+    scorers[player] += 1;
+  } else {
+    scorers[player] = 1;
+  }
+}
