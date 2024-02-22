f446deda8144a1a95544d48bc0b815a76a880c6a
sagarpal04 <sagarpal0402@gmail.com>
2024-02-22 08:50:33 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -1,101 +1,97 @@
 "use strict";
 
-// 110 Logical Assignment Operators
+// 111 Coding Challenge #1
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends �)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
 
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = "20:00",
-    address,
-  }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
+  printGoals(...playersScored) {
+    console.log(`${playersScored.length} goals were scored`);
+    console.log(...playersScored);
   },
 };
-
-const rest1 = {
-  name: "Capri",
-  numGuests: 0,
-};
-const rest2 = {
-  name: "La Piazza",
-  owner: "Giovanni Rossi",
-};
-// OR assignment operator
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
-// console.log(rest1.numGuests);
-// Output : 10
-// console.log(rest2.numGuests);
-// Output : 10
-
-// rest1.numGuests ||= 10;
-// rest2.numGuests ||= 10;
-// console.log(rest1.numGuests);
-// // Output: 10;
-// console.log(rest2.numGuests);
-// // Output: 10;
-
-// Nullish assignment operator
-// rest1.numGuests = rest1.numGuests ?? 10;
-// rest2.numGuests = rest2.numGuests ?? 10;
-// console.log(rest1.numGuests);
-// Output : 0
-// console.log(rest2.numGuests);
-// Output : 10
-
-// rest1.numGuests ??= 10;
-// rest2.numGuests ??= 10;
-// console.log(rest1.numGuests);
-// // Output: 0;
-// console.log(rest2.numGuests);
-// // Output: 10;
-
-// AND assignment operator
-// rest1.owner = rest1.owner && "<ANONYMOUS>";
-// rest2.owner = rest2.owner && "<ANONYMOUS>";
-// console.log(rest1.owner);
-// Output : undefined
-// console.log(rest2.owner);
-// Output: <ANONYMOUS>
-
-// rest1.owner &&= 10;
-// rest2.owner &&= 10;
-// console.log(rest1.owner);
-// Output: undefined
-// console.log(rest2.owner);
-// Output: <ANONYMOUS>
+// 1
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+// 2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+// 3
+const [allPlayers] = [...players1, ...players2];
+console.log(allPlayers);
+// 4
+const plater1Final = [...players1, "Thiago", "Coutinho", "Peristic"];
+console.log(plater1Final);
+// 5
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+// 6
+game.printGoals("Davies", "Muller", "Lewandowski", "Kinmich");
+game.printGoals(...game.scored);
+// 7
+game.odds.team1 > game.odds.team2 && console.log("Teams 1 Wins");
+game.odds.team1 < game.odds.team2 && console.log("Teams 2 Wins");
